5fc70c3e380bd2244812afca285ba6a81fe347d6
Maxim-Kazliakouski <75146035+Maxim-Kazliakouski@users.noreply.github.com>
2022-02-16 21:45:34 +0300

    Update conftest.py

--- Test/tests_AdminPage/conftest.py
+++ Test/tests_AdminPage/conftest.py
@@ -146,8 +146,17 @@ def browser(request):
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
             options.headless = True
-            s = Service('/Volumes/Work/Python_courses/Project/POM/Tools/chromedriver')
+             # params for docker
+            options = webdriver.ChromeOptions()
+            options.add_argument('--no-sandbox')
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+
+            s = Service('/usr/local/bin/chromedriver')
+#             s = Service('/Volumes/Work/TestProject/tools/chromedriver')
             browser = webdriver.Chrome(service=s, options=options)
+#             s = Service('/Volumes/Work/Python_courses/Project/POM/Tools/chromedriver')
+#             browser = webdriver.Chrome(service=s, options=options)
             # browser = webdriver.Chrome('/Volumes/Work/TestProject/tools/chromedriver', options=options)
             browser.maximize_window()
             browser.implicitly_wait(5)
