ea2ee7a024707d77e67dd4824a8358f0d48f6b64
Maxim Kazliakouski <max_kazliakouski@MacBook-Pro-Maxim.local>
2022-02-28 19:15:14 +0300

    adding changes to DB

--- DataBase/PostgreSQL.py
+++ DataBase/PostgreSQL.py
@@ -44,25 +44,6 @@ connection = create_connection(ConnectionToDB.DB_NAME,
                                ConnectionToDB.DB_HOST)
 
 
-# # data base creation
-# def create_database(connection, query):
-#     connection.autocommit = True
-#     cursor = connection.cursor()
-#     try:
-#         cursor.execute(query)
-#         print("Query executed successfully")
-#     except OperationalError as e:
-#         print(f"The error '{e}' occurred")
-#
-#
-# create_database_query = "CREATE DATABASE users"
-#
-#
-# # create_database(connection, create_database_query)
-#
-#
-
-
 # reading data from table
 def execute_read_query(connection, query):
     cursor = connection.cursor()
@@ -92,64 +73,8 @@ def parsing_response(request):
         return element
 
 
-# # -----------------------------------------------------------------------------------
-#
-# # creating table in database
-# create_users_table = """
-# CREATE TABLE IF NOT EXISTS test_users (
-#   id SERIAL PRIMARY KEY,
-#   name TEXT NOT NULL,
-#   age INTEGER,
-#   gender TEXT,
-#   nationality TEXT
-# )
-# """
-# # execute_query(connection, create_users_table)
-#
-# # -----------------------------------------------------------------------------------
-#
-# # inserting data into the table
-# users = [
-#     ("James", 25, "male", "USA"),
-#     ("Leila", 32, "female", "France"),
-#     ("Brigitte", 35, "female", "England"),
-#     ("Mike", 40, "male", "Denmark"),
-#     ("Elizabeth", 21, "female", "Canada"),
-# ]
-#
-# user_records = ", ".join(["%s"] * len(users))
-#
-# insert_query = (
-#     f"INSERT INTO test_users (name, age, gender, nationality) VALUES {user_records}"
-# )
-#
-# # connection.autocommit = True
-# # cursor = connection.cursor()
-# # cursor.execute(insert_query, users)
-#
-# # -----------------------------------------------------------------------------------
-#
-#
-# # getting data from table
-# select_users = "SELECT * FROM test_users"
-# users = execute_read_query(connection, select_users)
-#
-# for user in users:
-#     print(user)
-# # -----------------------------------------------------------------------------------
-#
-# query for outputting all tables in database
-
 tables_info = """
 SELECT n.nspname, c.relname
 FROM pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace
 WHERE c.relkind = 'r' AND n.nspname NOT IN('pg_catalog', 'information_schema');
 """
-# info = execute_read_query(connection, tables_info)
-# for each_row in info:
-#     print(each_row)
-
-# adding new group:
-# query = TestDataDB.QUERY_FOR_ADDING_GROUP
-# execute_query(connection, TestDataDB.QUERY_FOR_ADDING_GROUP)
-# execute_query(connection, TestDataDB.DELETING_NEW_GROUP)
